feb233deb6e808f8923aaf400d21639192a4f54a
Manjunatha Reddy PM <mreddypm@gmail.com>
2023-03-18 09:05:21 +0530

    Pratice

--- src/dailyparctice/useStateFunction.js
+++ src/dailyparctice/useStateFunction.js
@@ -10,8 +10,10 @@ import React, { Component, useState } from 'react'
 //     }
 
 //     increment=()=>{
-//     this.setState({
-//         count: this.state.count +1
+//     this.setState(previse => {
+    // return{
+//         count: previse.count +1
+    // }
 //     })
 //     }  
 //   render() {
@@ -39,33 +41,80 @@ import React, { Component, useState } from 'react'
 // }
 
 
-function UseStateFunction() {
-    let a =0
-    const [count, setCount]= useState(a)
-    const submit = ()=>{
-        setCount(a )
-    }
-    const submit1 = ()=>{
-        setCount((previse)=>
-                previse +  1
-        )
-    }
-    const submit2 = ()=>{
-        setCount((previse)=>
-                previse - 1
-        )
-    }
 
+
+// function UseStateFunction() {
+//     let a =0
+//     const [count, setCount]= useState(a)
+//     const submit = ()=>{
+//         setCount(a )
+//     }
+//     const submit1 = ()=>{
+//         setCount((previse)=>
+//                 previse +  1
+//         )
+//     }
+//     const submit2 = ()=>{
+//         setCount((previse)=>
+//                 previse - 1
+//         )
+//     }
+
+//     const increment5 = () => {
+//         for (let i = 0; i < 5; i++) {
+//             setCount(previse => previse + 1)
+            
+//         }
+        
+//     }
+
+//   return (
+//     <div>
+//         {count}
+//         <button onClick={submit}>click</button>
+//         <button onClick={submit1}>click</button>
+//         <button onClick={submit2}>click</button>
+//         <button onClick={increment5}>click</button>
+//     </div>
+//   )
+// }
+
+
+// function UseStateFunction() {
+//     const [name, setName] = useState({firstName:'', lastName:''})
+
+//   return (
+//     <form>
+//         <input type="text" vlaue={name.firstName} onChange={e=> setName({...name, firstName:e.target.value})}/>
+//         <input type="text" vlaue={name.lastName} onChange={e1=> setName({...name, lastName:e1.target.value})}/>
+//         <h2>FName -{name.firstName}</h2>
+//         <h2>lName -{name.lastName}</h2>
+//         <h2>{JSON.stringify(name)}</h2>
+//     </form>
+//   )
+// }
+
+
+
+function UseStateFunction() {
+    const [items, setItems] = useState([])
+
+    const addItem = () => {
+        setItems([...items,{
+            id: items.length,
+            value: Math.floor(Math.random() * 10) +1
+        }])
+    }
   return (
     <div>
-        {count}
-        <button onClick={submit}>click</button>
-        <button onClick={submit1}>click</button>
-        <button onClick={submit2}>click</button>
+        <button onClick={addItem}>Add a number</button>
+        <ul>
+           { items.map(item => (
+            <li Key={items.id}>{item.value}</li>))}
+        </ul>
     </div>
   )
 }
-
 
 
 export default UseStateFunction
